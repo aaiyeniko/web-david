ee3fdab50d5f96a02933a47414aa1a692b11cf17
David Aiyeniko <aaiyeniko@gmail.com>
2021-11-24 23:45:16 +0100

    pushing files to remote

---
+++ product.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Tennis",
+        "description": "tennis is a 2 person <b> sport  </b> which is wonderful",
+        "price": 220,
+        "image": "images/tennis.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 2,
+    },
+    {
+        "id": 1002,
+        "title": "Football",
+        "description": "football is aboustly the best<b> sport</b> in the World",
+        "price": 250,
+        "image": "images/football 3.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 4,
+    },
+    {
+        "id": 1003,
+        "title": "Hockey",
+        "description": "hockey is general sport  <b> that is  </b>  excellent",
+        "price": 310,
+        "image": "images/hockey2.jpg",
+        "availableInventory": 10, //was 5
+        "rating": 5,
+    },
+
+]
